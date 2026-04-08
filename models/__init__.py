db7b6875f55df5ef005e00ff8cf717146dce3abb
Sharon Vico <46236197@alumnos.frsr.utn.edu.ar>
2026-04-07 23:02:39 -0300

    Elimino estructura anterior, debido a que no habia instalado las skills

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Paquete models - Contiene las entidades y esquemas de datos.
-"""
-
-from models.user import User, UserCreate, UserResponse, UserUpdate
-
-__all__ = ["User", "UserCreate", "UserResponse", "UserUpdate"]
