16a29e350e7d9fcf580c9ad6f0c7e1fe0f94ac83
Sharon Vico <46236197@alumnos.frsr.utn.edu.ar>
2026-04-30 16:21:53 -0300

    Refactorizaciónde archivos health.py y test.py

--- app/routes/test.py
+++ app/routes/test.py
@@ -5,7 +5,11 @@ from app.services.test_service import guardar
 
 router = APIRouter()
 
+TEST_ENTITY_NAME = "test"
+TEST_SUCCESS_MESSAGE = "guardado"
+
+
 @router.get("/test", response_model=TestResponse)
-def test():
-    guardar("test")
-    return {"msg": "guardado"}
+def save_test_entry():
+    guardar(TEST_ENTITY_NAME)
+    return {"msg": TEST_SUCCESS_MESSAGE}
