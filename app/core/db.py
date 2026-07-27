fa82a59d555ef57340247ba0681b3073a5d97298
Sharon Vico <46236197@alumnos.frsr.utn.edu.ar>
2026-07-27 04:06:24 -0300

    Agrego manejo de variables de entorno para MongoDB

--- app/core/db.py
+++ app/core/db.py
@@ -6,7 +6,11 @@ from app.core.logger import logger
 
 load_dotenv()
 
-MONGO_URL = os.getenv("MONGO_URL")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+
+MONGO_DATABASE = os.getenv( "MONGO_DATABASE", "mi_base")
+
+
 client = MongoClient(MONGO_URL)
 db = client["mi_base"]
 
